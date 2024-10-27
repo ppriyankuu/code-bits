e6820e2ebbe223def2f59bc34f60d2132b131550
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-10-27 21:22:43 +0530

    prisma client singleton

--- lib/db.ts
+++ lib/db.ts
@@ -8,6 +8,7 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
+// exporting the prisma client
 export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
