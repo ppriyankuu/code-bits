0360791e09271306bfc5d685d2baf06593421af2
Priyanku Gogoi <workwithpriyanku@gmail.com>
2024-11-17 14:48:36 +0530

    logo component

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,9 +3,9 @@ import logo from '@/public/logo.svg'
 
 export const Logo = () => {
     return (
-        <div className="w-[8rem] flex items-center gap-1">
+      <div className="w-[8rem] flex items-center gap-1">
          <Image src={ logo } alt="logo" className="w-full" width={ 200 } />
          <p className="text-[#8046FD] font-semibold text-xl">CodeBits</p>
-        </div>
+      </div>
     )
 }
